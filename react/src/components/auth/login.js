ee9a2e06663569f0f277a59ad627962709616d4d
tylerwhyler <tyjiman@gmail.com>
2020-07-16 16:10:06 -0700

    added the POST request for the devcamp space

--- react/src/components/auth/login.js
+++ react/src/components/auth/login.js
@@ -1,13 +1,59 @@
 import React from "react";
+import axios from "axios";
 
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit(event) {
+        axios.post("https://api.devcamp.space/sessions",
+            {
+                client: {
+                    email: this.state.email,
+                    password: this.state.password
+                }
+            }, { withCredentials: true }
+        )
+        .then(res => {
+            console.log("response", res);
+        });
+        event.preventDefault()
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>LOGIN TO ACCES YOUR DASHBOARD</h1>
-                <form>
-                    <input type="text" />
-                    <input type="password" />
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Your email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <input type="password" 
+                        type="password"
+                        name="password"
+                        placeholder="Your password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Login</button>
                 </form>
             </div>
         )
