e334e88cd4b2c416469c5699ade9e329acf759ee
tylerwhyler <tyjiman@gmail.com>
2020-07-21 12:27:21 -0700

    added devcamp.space API items to portfolio manager page

--- react/src/components/pages/portfolio-manager.js
+++ react/src/components/pages/portfolio-manager.js
@@ -1,7 +1,35 @@
 import React from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list"
 
 export default class PortfolioManager extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+        axios.get("https://tyji.devcamp.space/portfolio/portfolio_items", { withCredentials: true})
+        .then(res => {
+            console.log(res)
+            this.setState({
+                portfolioItems: [...res.data.portfolio_items]
+            })
+        }) .catch(err => {
+            console.log("getportfoliotitems", err)
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
     render() {
+        // console.log(this.state.portfolioItems.length > 0 ? this.state.portfolioItems[0].id : "NO portfolio Items")
         return (
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
@@ -11,7 +39,7 @@ export default class PortfolioManager extends React.Component {
                 </div>
                 <div className="right-column">
                     <h1>
-                        Column 2
+                        <PortfolioSidebarList data={this.state.portfolioItems} />
                     </h1>
                 </div>
             </div>
