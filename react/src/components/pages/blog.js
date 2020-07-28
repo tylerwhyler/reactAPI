14644afc2c13393c2eed54092a8f092453f63062
tylerwhyler <tyjiman@gmail.com>
2020-07-28 12:36:56 -0700

    added ability to render the blog items from devcamp space api

--- react/src/components/pages/blog.js
+++ react/src/components/pages/blog.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios"
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
-      <Link to="/about-me"> Read more about me.</Link>
-    </div>);
+export default class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this)
+  }
+
+  getBlogItems() {
+    axios.get("https://tyji.devcamp.space/portfolio/portfolio_blogs", { withCredentials: true })
+    .then (res => {
+      this.setState({
+        blogItems: res.data.portfolio_blogs
+      })
+    }) .catch (err => {
+      console.error(err)
+    })
+  }
+
+  componentDidMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <h1>{blogItem.title}</h1>
+    })
+    return (
+      <div>
+        {blogRecords}
+      </div>
+    );
+  }
 }
