65e16c55fb670d56800ab371db8b34bab2f3a9dd
tylerwhyler <tyjiman@gmail.com>
2020-07-29 12:16:40 -0700

    added more functionality to blog posts

--- react/src/components/pages/blog.js
+++ react/src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from "axios"
 
 import BlogItem from "./../blog/blog-item"
@@ -9,17 +10,39 @@ export default class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true
     }
 
     this.getBlogItems = this.getBlogItems.bind(this)
+    this.activateInfiniteScroll();
+  }
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+        return;
+      }
+      if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+        this.getBlogItems();
+      }
+    }
+
+    
   }
 
   getBlogItems() {
-    axios.get("https://tyji.devcamp.space/portfolio/portfolio_blogs", { withCredentials: true })
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    })
+    axios.get(`https://tyji.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, { withCredentials: true })
     .then (res => {
       this.setState({
-        blogItems: res.data.portfolio_blogs
+        blogItems: this.state.blogItems.concat(res.data.portfolio_blogs),
+        totalCount: res.data.meta.total_records,
+        isLoading: false
       })
     }) .catch (err => {
       console.error(err)
@@ -37,6 +60,11 @@ export default class Blog extends Component {
     return (
       <div className="blog-container">
         <div className="content-container">{blogRecords}</div>
+
+        {this.state.isLoading ? (
+        <div className="content-loader">
+          <FontAwesomeIcon icon="spinner" spin />
+        </div> ) : null}
       </div>
     )
   }
