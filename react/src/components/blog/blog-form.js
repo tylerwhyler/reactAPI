fd565b3964bce4d209f9028868b70b86cf92ef70
tylerwhyler <tyjiman@gmail.com>
2020-07-31 13:50:46 -0700

    added dropzone to blog form modal

--- react/src/components/blog/blog-form.js
+++ react/src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import DropZoneComponent from "react-dropzone-component";
 
 import RichTextEditor from "../forms/rich-text-editor"
 
@@ -10,12 +11,38 @@ export default class BlogForm extends React.Component {
         this.state = {
             title: "",
             blog_status: "",
-            content: ""
+            content: "",
+            featured_image: ""
         }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this)
+
+        this.componentConfig = this.componentConfig.bind(this)
+        this.djsConfig = this.djsConfig.bind(this)
+        this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this)
+    }
+
+    handleFeaturedImageDrop() {
+        return {
+            addedfile: file => this.setState({ featured_image: file })
+        }
+    }
+
+    djsConfig() {
+        return {
+            addRemoveLinks: true,
+            maxFiles: 1
+        }
+    }
+
+    componentConfig() {
+        return {
+            iconFiletypes: [".jpg", ".png"],
+            showFiletypeIcon: true,
+            postUrl: "https://httpbin.org/post"
+        }
     }
 
     handleRichTextEditorChange(content) {
@@ -41,7 +68,7 @@ export default class BlogForm extends React.Component {
                 blog_status: "",
                 content: ""
             })
-            
+
             this.props.handleSuccessfulFormSubmission(res.data.portfolio_blog)
 
         }).catch(err => {
@@ -80,6 +107,16 @@ export default class BlogForm extends React.Component {
                     <RichTextEditor 
                         handleRichTextEditorChange={this.handleRichTextEditorChange}
                     />
+                </div>
+
+                <div className="image-uploaders">
+                    <DropZoneComponent 
+                    config={this.componentConfig()}
+                    djsConfig={this.djsConfig()}
+                    eventHandlers={this.handleFeaturedImageDrop()}
+                    >
+                        <div className="dz-message">Featured Image</div>
+                    </DropZoneComponent>
                 </div>
                 
                 <button className="btn">Save</button>
